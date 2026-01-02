c574b781742c6a295ff113810d1f400b3ee49b52
Aldo Camacho <me@aldocamacho.com>
2026-01-01 22:14:16 -0800

    connect the parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/Quak1/learn-http-go/internal/request"
 )
 
 const filename = "messages.txt"
@@ -27,50 +26,14 @@ func main() {
 
 		fmt.Println("A connection has been accepted")
 
-		ch := getLinesChannel(conn)
-		for line := range ch {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		fmt.Println("-----------Connection has been closed----------")
+		fmt.Println("Request line:")
+		fmt.Println("- Method:", req.RequestLine.Method)
+		fmt.Println("- Target:", req.RequestLine.RequestTarget)
+		fmt.Println("- Version:", req.RequestLine.HttpVersion)
 	}
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	ch := make(chan string)
-
-	go func() {
-		defer close(ch)
-		defer f.Close()
-
-		line := ""
-		for {
-			buffer := make([]byte, 8)
-			_, err := f.Read(buffer)
-			if err != nil {
-				if line != "" {
-					ch <- line
-				}
-
-				if errors.Is(err, io.EOF) {
-					return
-				}
-
-				fmt.Printf("error")
-				return
-			}
-
-			parts := strings.Split(string(buffer), "\n")
-
-			for _, part := range parts[:len(parts)-1] {
-				line += part
-				ch <- line
-				line = ""
-			}
-
-			line += parts[len(parts)-1]
-		}
-	}()
-
-	return ch
 }
