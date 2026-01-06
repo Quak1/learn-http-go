54fa624379f896955bace20528677c3847ce5912
Aldo Camacho <me@aldocamacho.com>
2026-01-06 14:46:20 -0800

    response handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,20 +1,38 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"sync/atomic"
 
+	"github.com/Quak1/learn-http-go/internal/request"
 	"github.com/Quak1/learn-http-go/internal/response"
 )
 
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
+
+func (h HandlerError) Write(w io.Writer) {
+	response.WriteStatusLine(w, h.StatusCode)
+	headers := response.GetDefaultHeaders(len(h.Message))
+	response.WriteHeaders(w, headers)
+	w.Write([]byte(h.Message))
+}
+
 type Server struct {
+	handler      Handler
 	listener     net.Listener
 	serverClosed atomic.Bool
 }
 
-func Serve(port int) (*Server, error) {
+func Serve(port int, handler Handler) (*Server, error) {
 	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return nil, err
@@ -22,6 +40,7 @@ func Serve(port int) (*Server, error) {
 
 	s := Server{
 		listener: ln,
+		handler:  handler,
 	}
 
 	go s.listen()
@@ -53,16 +72,36 @@ func (s *Server) listen() {
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
 
-	err := response.WriteStatusLine(conn, response.StatusOK)
+	req, err := request.RequestFromReader(conn)
+	if err != nil {
+		requestError := &HandlerError{
+			StatusCode: response.StatusBadRequest,
+			Message:    err.Error(),
+		}
+		requestError.Write(conn)
+		return
+	}
+
+	var buff bytes.Buffer
+	handlerError := s.handler(&buff, req)
+	if handlerError != nil {
+		handlerError.Write(conn)
+		return
+	}
+
+	err = response.WriteStatusLine(conn, response.StatusOK)
 	if err != nil {
 		log.Println("Error: couldn't write status line.", err)
 		return
 	}
 
-	headers := response.GetDefaultHeaders(0)
+	b := buff.Bytes()
+	headers := response.GetDefaultHeaders(len(b))
 	err = response.WriteHeaders(conn, headers)
 	if err != nil {
 		log.Println("Error: couldn't write headers.", err)
 		return
 	}
+
+	conn.Write(b)
 }
